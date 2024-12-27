3db27eb4c5d5b0807bd09c62d7ad2535a7489a3c
Dhanesh260799 <b.dhaneshkumar07@gmail.com>
2024-12-27 22:49:45 +0530

    Updated Standalone testfile

--- src/test/java/danish/tests/standAlone.java
+++ src/test/java/danish/tests/standAlone.java
@@ -20,7 +20,7 @@ public class standAlone {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		// WebDriverManager.chromedriver().setup();
-
+//ci/cd modified
 		// implicit wait
 		// used for sync issues
 		String productName = "ZARA COAT 3";
